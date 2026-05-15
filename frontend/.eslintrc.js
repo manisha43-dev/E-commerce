67e628720b5fa95a916f69aeba1ac6cfa2d02583
manisha43-dev <manishakumar2507@gmail.com>
2026-05-15 15:17:43 +0530

    fix warning

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    "eqeqeq": "warn",        // fixes === errors
+    "react-hooks/exhaustive-deps": "warn",  // fixes useEffect warning
+    "no-unused-vars": "warn"  // fixes assigned but unused
+  }
+}
