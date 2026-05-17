082099a18e59e742a8200af1087a9cbcc7e97825
manisha43-dev <manishakumar2507@gmail.com>
2026-05-17 11:28:05 +0530

    fix: remove config.env from tracking

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URL).then((con)=>{
+    mongoose.connect(process.env.MONGODB_URI).then((con)=>{
         console.log(`MongoDB connected to host:`+con.connection.host);     
     });
 };
